7b600b0a5cc1e627ebc64482b369abe8c6ddc5b9
joelbeya <32399947+joelbeya@users.noreply.github.com>
2020-01-30 02:14:23 +0100

    final demo

--- www/js/controllers.js
+++
@@ -1,9 +0,0 @@
-angular.module('ionic-demo.controllers', [])
-
-    .controller('DashCtrl', function ($scope) {
-    })
-
-    .controller('MenuCtrl', function ($scope, MenuService) {
-        $scope.menu = MenuService.getMenu();
-    });
-
